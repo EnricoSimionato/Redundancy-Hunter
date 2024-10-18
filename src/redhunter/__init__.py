9dc0c96928cd5e10f9c0c3a5c73621a83fe817a6
enricosimionato <enrico.simionato@mail.polimi.it>
2024-10-18 09:27:18 +0200

    Init for redhunter and toml file for the project

---
+++ src/redhunter/__init__.py
@@ -0,0 +1,6 @@
+__all__ = [
+    "LayerReplacingModelWrapper",
+    "NullLayerReplacingModelWrapper"
+]
+
+from redhunter.analysis.layer_replacement_analysis_utils import LayerReplacingModelWrapper, NullLayerReplacingModelWrapper
