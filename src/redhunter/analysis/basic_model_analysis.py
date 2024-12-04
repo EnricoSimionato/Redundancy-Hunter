018b3cfb4c13e7eb287e2ad122c1af555d077f60
enricosimionato <enrico.simionato@mail.polimi.it>
2024-12-04 15:42:45 +0100

    Basic information model analysis

--- src/redhunter/analysis/basic_model_analysis.py
+++ src/redhunter/analysis/basic_model_analysis.py
@@ -1,133 +1,86 @@
-import sys
+from typing import Any, override
 
 import transformers
 
-from exporch import Verbose
-
+from exporch import Config
 from exporch.utils.causal_language_modeling import load_model_for_causal_lm
 
-from redhunter.analysis.analysis_utils import extract
+from redhunter.analysis.analysis_utils import extract, AnalysisTensorDict
+from redhunter.analysis_experiment import AnalysisExperiment
 
 
-def analyze_layers(
-        model: transformers.PreTrainedModel,
-        paths_layers_to_analyze: list = (),
-        black_list: list = (),
-        verbose: Verbose = Verbose.INFO
-) -> None:
+class ModelBasicAnalysis(AnalysisExperiment):
     """
-    Analyze the layers of a model.
-
-    Args:
-        model (transformers.PreTrainedModel):
-            The model.
-        paths_layers_to_analyze (list, optional):
-            The paths of the layers to analyze. Defaults to ().
-        black_list (list, optional):
-            The black list. Defaults to ().
-        verbose (Verbose, optional):
-            The verbosity level. Defaults to Verbose.INFO.
+    The class for the model basic analysis.
     """
 
-    extracted_layers = []
-    extract(
-        model,
-        paths_layers_to_analyze,
-        extracted_layers,
-        black_list=black_list,
-        verbose=verbose
-    )
-    seen_labels = set()
+    @override
+    def _run_experiment(
+            self
+    ) -> None:
+        """
+        Runs the experiment.
+        """
 
-    for layer in extracted_layers:
-        if layer["label"] in seen_labels:
-            continue
-        print("Printing the name of the layer:")
-        print(f"    {layer['name']}")
-        print("Printing the path of the layer:")
-        print(f"    {layer['path']}")
-        print("Printing the layer:")
-        print(f"    {layer['layer']}")
-        print("Printing the shape of the layer's weight")
-        print(f"    {layer['weight'].shape}")
-        # print("Printing the layer's weight")
-        # print(f"  {layer['weight']}")
-        print("##################################################")
+        # Loading the model
+        model = load_model_for_causal_lm(config=self.get_config())
+        # Analyzing the model
+        self.analyze_model(model, self.get_config().get("targets", ()), self.get_config().get("black_list", ()))
 
-        seen_labels.add(layer["label"])
+    def analyze_model(
+            self,
+            model: transformers.PreTrainedModel | transformers.AutoModel,
+            target_layers: list = (),
+            black_list: list = ()
+    ) -> None:
+        """
+        Analyzes a model.
 
+        Args:
+            model (transformers.PreTrainedModel):
+                The model.
+            target_layers (list, optional):
+                The paths of the layers to analyze. Defaults to ().
+            black_list (list, optional):
+                The black list. Defaults to ().
+        """
 
-def analyze_model(
-        model_id: str,
-        dtype: str = "float32",
-        quantization: str = None,
-        paths_layers_to_analyze: list = (),
-        black_list: list = (),
-        verbose: Verbose = Verbose.INFO
-) -> None:
-    """
-    Analyzes a model.
+        self.log(f"Model loaded: \n\n{model}")
 
-    Args:
-        model_id (str):
-            The model ID.
-        dtype (str, optional):
-            The data type. Defaults to "float32".
-        quantization (str, optional):
-            The quantization. Defaults to "4bit".
-        paths_layers_to_analyze (list, optional):
-            The paths of the layers to analyze. Defaults to ().
-        black_list (list, optional):
-            The black list. Defaults to ().
-        verbose (Verbose, optional):
-            The verbosity level. Defaults to Verbose.INFO.
-    """
+        extracted_layers = AnalysisTensorDict()
+        extract(model, target_layers, extracted_layers, black_list=black_list)
 
-    model = load_model_for_causal_lm(model_id, dtype, quantization)
+        for key in extracted_layers.get_keys():
+            layers = extracted_layers.get_tensor_list(key)
+            for layer in layers:
+                print("Printing the name of the layer:")
+                print(f"    {layer.get_name()}")
+                print("Printing the path of the layer:")
+                print(f"    {layer.get_path()}")
+                print("Printing the layer:")
+                print(f"    {layer.get_layer()}")
+                print("Printing the shape of the layer's weight")
 
-    print(model)
+                print("------------------------------------------------------------------------------------------")
 
-    analyze_layers(
-        model,
-        paths_layers_to_analyze,
-        black_list=black_list,
-        verbose=verbose
-    )
+    def _postprocess_results(
+            self
+    ) -> None:
+        """
+        Postprocess the results.
+        It does nothing.
+        """
 
+        pass
 
-def main():
-    """
-    Main function to analyze a model.
-    """
+    def _plot_results(
+            self,
+            config: Config,
+            data: Any
+    ) -> None:
+        """
+        Plots the results.
+        It does nothing.
+        """
 
-    if len(sys.argv) < 3:
-        print(
-            "Usage: python3 basic_model_analysis.py <model_id> --targets <path_to_layer_1> <path_to_layer_2> ... --black_list <black_listed_string_1> <black_listed_string_2> ...")
-        sys.exit(1)
-    if "--targets" not in sys.argv:
-        print(
-            "Usage: python3 basic_model_analysis.py <model_id> --targets <path_to_layer_1> <path_to_layer_2> ... --black_list <black_listed_string_1> <black_listed_string_2> ...")
-
-    if "--black_list" not in sys.argv:
-        print("Warning: No black list provided.")
-        end_targets = len(sys.argv)
-    else:
-        end_targets = sys.argv.index("--black_list")
-
-    model_to_analyze = sys.argv[1]
-    paths_layers_to_analyze = sys.argv[sys.argv.index("--targets"):end_targets]
-    if "--black_list" not in sys.argv:
-        black_list = []
-    else:
-        black_list = sys.argv[end_targets + 1:]
-
-    analyze_model(
-        model_to_analyze,
-        paths_layers_to_analyze=paths_layers_to_analyze,
-        black_list=black_list,
-        verbose=Verbose.INFO
-    )
-
-
-if __name__ == "__main__":
-    main()
+        pass
